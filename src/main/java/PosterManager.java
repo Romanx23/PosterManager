353b2364d85543740587132dc051113cd30f5ca1
Роман Панченко <romanx23@yandex.ru>
2022-10-12 21:17:18 +0300

    New repo

---
+++ src/main/java/PosterManager.java
@@ -0,0 +1,36 @@
+public class PosterManager {
+    private static String[] movies = new String[0];
+    private static int limit;
+    public PosterManager(int limit) {
+        this.limit = limit;
+    }
+
+    public PosterManager() {
+
+        limit = 10;
+    }
+
+
+    public void add(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    public static String[] findAll() {
+
+        return movies;
+    }
+
+    public static String[] findLast() {
+        int resultSize = limit < movies.length ? limit : movies.length;
+        String[] result = new String[resultSize];
+        for (int i = 0; i < resultSize; i++){
+            result[i] = movies[movies.length - 1 - i];
+        }
+        return result;
+    }
+}
